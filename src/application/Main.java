661994012dbe08ab5b13c77eb555afd6762858bc
djebbar <seddikdjaber4@gmail.com>
2025-04-07 18:27:28 +0100

    add main

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,6 @@ public class Main extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("/vue/A.fxml"));
 			Scene scene = new Scene(root);
 			
-			
 			stage.setScene(scene);
 			stage.show();
 			
