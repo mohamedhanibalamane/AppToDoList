23d89e09a40abad00610e26d26e2e1b5f2b74586
djebbar <seddikdjaber4@gmail.com>
2025-04-09 18:14:40 +0100

    ajouter class task et user

---
+++ src/model/User.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.*;
+
+
+public class User {
+	private int ID;
+	private String name;
+	private String prenome;
+	
+	
+	
+	
+
+LinkedList<Task> listoftasks = new LinkedList <Task> ();
+LinkedList<Task> historiquelist = new LinkedList <Task> ();
+LinkedList<Task> corbielist = new LinkedList <Task> ();
+	
+	
+	
+	
+	
+	
+	
+	
+}
